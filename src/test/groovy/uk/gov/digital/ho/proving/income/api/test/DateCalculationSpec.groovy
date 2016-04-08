f98000018b9a5bafcf3178ea0801398a44f0b2d7
Andy Birkett <abirkett13@gmail.com>
2016-04-08 13:37:11 +0100

    [AB] SD67 - Spock tests for defect

---
+++ src/test/groovy/uk/gov/digital/ho/proving/income/api/test/DateCalculationSpec.groovy
@@ -0,0 +1,114 @@
+package uk.gov.digital.ho.proving.income.api.test
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import spock.lang.Specification
+import uk.gov.digital.ho.proving.income.api.FinancialCheckValues
+import uk.gov.digital.ho.proving.income.api.IncomeValidator
+import uk.gov.digital.ho.proving.income.domain.Applicant
+import uk.gov.digital.ho.proving.income.domain.Income
+import uk.gov.digital.ho.proving.income.domain.IncomeProvingResponse
+import uk.gov.digital.ho.proving.income.domain.Link
+
+class DateCalculationSpec extends Specification {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(IncomeValidatorSpec.class);
+
+    final String PIZZA_HUT = "Pizza Hut"
+    final String BURGER_KING = "Burger King"
+
+    def "Check we get 1 month difference between 23/01/2015 and 23/02/2015"() {
+
+        given:
+        Date from = getDate(2015,01,23)
+        Date to = getDate(2015,02,23)
+
+        when:
+        long months = IncomeValidator.getDifferenceInMonthsBetweenDates(to, from)
+
+        then:
+        months == 1
+
+    }
+
+    def "Check we get 1 month difference between 14/07/2015 and 17/06/2015"() {
+
+        given:
+        Date from = getDate(2015,06,17)
+        Date to = getDate(2015,07,14)
+
+        when:
+        long months = IncomeValidator.getDifferenceInMonthsBetweenDates(to, from)
+
+        then:
+        months == 1
+
+    }
+
+
+    def "Check we get 1 month difference between 23/01/2015 and 25/02/2015"() {
+
+        given:
+        Date from = getDate(2015,01,23)
+        Date to = getDate(2015,02,20)
+
+        when:
+        long months = IncomeValidator.getDifferenceInMonthsBetweenDates(to, from)
+
+        then:
+        months == 1
+
+    }
+
+    def "Check we get 1 month difference between 1/01/2015 and 28/02/2015"() {
+
+        given:
+        Date from = getDate(2015,01,1)
+        Date to = getDate(2015,02,28)
+
+        when:
+        long months = IncomeValidator.getDifferenceInMonthsBetweenDates(to, from)
+
+        then:
+        months == 1
+
+    }
+
+
+    def "Check we get 2 month difference between 1/12/2014 and 28/02/2015"() {
+
+        given:
+        Date from = getDate(2014,12,1)
+        Date to = getDate(2015,02,28)
+
+        when:
+        long months = IncomeValidator.getDifferenceInMonthsBetweenDates(to, from)
+
+        then:
+        months == 2
+
+    }
+
+    def "Check we get 14 month difference between 1/12/2014 and 28/02/2016"() {
+
+        given:
+        Date from = getDate(2014,12,1)
+        Date to = getDate(2016,02,28)
+
+        when:
+        long months = IncomeValidator.getDifferenceInMonthsBetweenDates(to, from)
+
+        then:
+        months == 14
+
+    }
+
+    Date getDate(int year, int month, int day) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, month);
+        cal.set(Calendar.DATE, day);
+        return cal.getTime()
+    }
+
+}
