7c5c238dbb7bff154884d0dfdc6f2d4294a315a2
jon-shanks <jon.shanks@gmail.com>
2016-06-22 13:46:58 +0100

    adding in changes to remove the dockerfiles and also rename script from build to build_docker

---
+++ build_docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+[ -n "${DEBUG}" ] && set -x
+
+GRADLE_IMAGE="quay.io/ukhomeofficedigital/gradle:v2.13.5"
+GIT_COMMIT=${GIT_COMMIT:-$(git rev-parse --short HEAD)}
+VERSION="0.1.0"
+
+build() {
+
+  MOUNT="${PWD}:/code"
+  [ -d "${HOME}/.gradle/caches" ] && MOUNT="${MOUNT} -v ${HOME}/.gradle/caches:/root/.gradle/caches"
+
+  [ -d "${HOME}/.m2/repository" ] && MOUNT="${MOUNT} -v ${HOME}/.m2/repository:/root/.m2/repository"
+
+  docker run -e GIT_COMMIT=${GIT_COMMIT} -e VERSION=${VERSION} -v ${MOUNT} "${GRADLE_IMAGE}" "${@}"
+}
+
+build "${@}"
