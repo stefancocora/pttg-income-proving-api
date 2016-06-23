fc034033615f3cf5ebd37f86aa4210bfdff372a5
jon-shanks <jon.shanks@gmail.com>
2016-06-23 13:04:43 +0100

    addin gin changes for build gradle and the build_docker to reflect buliding with gradle properties

--- build_docker.sh
+++ build_docker.sh
@@ -10,9 +10,14 @@ VERSION="0.1.0"
 build() {
 
   MOUNT="${PWD}:/code"
+  # Mount in the local gradle cache into the docker container
   [ -d "${HOME}/.gradle/caches" ] && MOUNT="${MOUNT} -v ${HOME}/.gradle/caches:/root/.gradle/caches"
 
+  # Mount in local maven repository into the docker container
   [ -d "${HOME}/.m2/repository" ] && MOUNT="${MOUNT} -v ${HOME}/.m2/repository:/root/.m2/repository"
+  
+  # Mount in local gradle user directory
+  [ -d "${HOME}/.gradle" ] && MOUNT="${MOUNT} -v ${HOME}/.gradle:/root/.gradle"
 
   docker run -e GIT_COMMIT=${GIT_COMMIT} -e VERSION=${VERSION} -v ${MOUNT} "${GRADLE_IMAGE}" "${@}"
 }
