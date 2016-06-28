d325e9ac49790429ca1fcf303db23ed4d0555245
jon-shanks <jon.shanks@gmail.com>
2016-06-28 11:31:39 +0100

    adding in changes to the run script

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -x
+NAME=${NAME:-pttg-income-proving-api}
 
-JAR=$(find . -name pttg-family-migration-api-*.jar|head)
+JAR=$(find . -name ${NAME}*.jar|head -1)
 java -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
