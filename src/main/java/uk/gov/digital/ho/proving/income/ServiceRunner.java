1449a953d9f46bff7f31f3487663a94bbaa201d8
atodd <alistair.todd@digital.homeoffice.gov.uk>
2016-08-22 12:16:04 +0100

    Fix for serving apidocs content from service root

--- src/main/java/uk/gov/digital/ho/proving/income/ServiceRunner.java
+++ src/main/java/uk/gov/digital/ho/proving/income/ServiceRunner.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class ServiceRunner {
 
